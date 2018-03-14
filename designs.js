6e72279d960ca75cbe7dc723dfc330a5bdebb3db
Jon Arild Karlsen <karlsen.jonarild@gmail.com>
2018-03-14 12:38:59 +0100

    Call makeGrid on size submit

--- designs.js
+++ designs.js
@@ -1,8 +1,3 @@
-// Prevent submission in order to keep height/width values
-document.querySelector('#sizePicker').addEventListener('click', function(e){
-	e.preventDefault();
-});
-
 // Select color input
 const formElementColor = document.querySelector('#colorPicker');
 
@@ -13,3 +8,11 @@ const formElementWidth = document.querySelector('#inputWidth');
 // Draw grid
 function makeGrid() {
 }
+
+// When size is submitted by the user, call makeGrid()
+document.querySelector('#sizePicker').addEventListener('submit', makeGrid());
+
+// Prevent submission in order to keep height/width values
+document.querySelector('#sizePicker').addEventListener('click', function(e){
+	e.preventDefault();
+});
